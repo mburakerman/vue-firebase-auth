81b327e81a9791c90aa2d9c3ca24eddc1ca1fb59
Mehmet Burak Erman <mburakerman@gmail.com>
2020-04-01 10:45:05 +0300

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,6 @@
             <p>
               <strong>T I L</strong> by
               <a href="https://mburakerman.github.io">Mehmet Burak Erman</a>
-              .
             </p>
             <p>Copyright 2020</p>
           </div>
